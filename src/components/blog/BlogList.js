70b697c672f9077c96eed086cc50f5c4a2e75187
Yury Karalkou <yury.karalkou@gmail.com>
2018-02-17 17:13:26 +0300

    [YK] Start to create blog components

---
+++ src/components/blog/BlogList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class BlogList extends Component {
+    render() {
+        return (
+            <div>Blog</div>
+        );
+    }
+}
+
+BlogList.propTypes = {};
+BlogList.defaultProps = {};
+
+export default BlogList;
---
+++ src/components/blog/BlogListFiltrationPanel.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class BlogListFiltrationPanel extends Component {
+    render() {
+        return (
+            <div>BlogListFiltrationPanel</div>
+        );
+    }
+}
+
+BlogListFiltrationPanel.propTypes = {};
+BlogListFiltrationPanel.defaultProps = {};
+
+export default BlogListFiltrationPanel;
